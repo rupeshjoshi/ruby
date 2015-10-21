ac3982fb5488083b30889f6e2a12fa858e186eac
rupeshjoshi <rupesh.joshi@berylsystems.com>
2015-10-21 18:29:08 +0530

    hash

---
+++ programs/hash1.rb
@@ -0,0 +1,6 @@
+names = Hash.new
+names[1] = "Jane"
+names[2] = "Thomas"
+
+puts names
+
---
+++ programs/hash2.rb
@@ -0,0 +1,7 @@
+
+names = Hash.new
+names.store(1, "Jane")
+names.store(2, "Thomas")
+names.store(3, "Rebecca")
+
+puts names
